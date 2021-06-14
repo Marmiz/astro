6bfce83111286d853e5c99ac8a26b24264cb317d
Claudio.Restifo <claudio.restifo@eu.jll.com>
2021-06-14 20:59:51 +0200

    add buildoption.site to docs example

--- examples/docs/astro.config.mjs
+++ examples/docs/astro.config.mjs
@@ -1,4 +1,8 @@
 export default {
+  buildOptions: {
+    // site: '',            // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: false, // Generate sitemap (set to "false" to disable)
+  },
   renderers: [
     '@astrojs/renderer-preact'
   ]
